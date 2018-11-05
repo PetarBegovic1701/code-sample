bdb025ca870b280432cfaa740802733066bba435
Petar <petar.begovic1701@gmail.com>
2018-11-05 01:09:32 +0100

    Tab and spaces made the code unreadable

--- src/TeamMembers.js
+++ src/TeamMembers.js
@@ -3,8 +3,8 @@ import './TeamMembers.css';
 import {persons} from './fakeData';
 
 class TeamMembers extends Component {
-	// Yes, here, the state management is redundant
-	// but serves a purpose for the demo
+  // Yes, here, the state management is redundant
+  // but serves a purpose for the demo
   constructor(props) {
     super(props);
     this.state = {
@@ -12,15 +12,15 @@ class TeamMembers extends Component {
     };
   }
 
-	componentWillMount = () => {
-		this.setState({
-			team: persons
-		}, () => {
-			console.log(this.state.team)
-			// nested arrow function because 
-			// the setState is asynchronous
-		});	
-	}
+  componentWillMount = () => {
+    this.setState({
+      team: persons
+    }, () => {
+      console.log(this.state.team)
+      // nested arrow function because 
+      // the setState is asynchronous
+    }); 
+  }
 
   render() {
     const { team } = this.state;
@@ -28,76 +28,72 @@ class TeamMembers extends Component {
     // reusing "this.state", and makes the code more readable
 
     return (
-			<div className="teamHolder">
-				{team.map((member, index) => (
-					<div className="teamColumn members" key={index}>
-						<div className="memberContainer">
-							<div className="avatarContainer">
-								<img src={member.image} alt="" className="avatar" />
-							</div>
-								<div className="memberFullname">
-									{member.fullname.map((name, index) => (
-										<h2 className="memberFullnameMargin" key={index}>
-											{ name }
-										</h2>
-									))}
-								</div>
-								<div className="memberPositionCenter">
-									{member.positions && member.positions.map((position, index) => (
-										<p className="memberPositionRow" key={index}>
-											{ position }
-										</p>
-									))}
-								</div>
-								<p className="memberInfo">
-									{ member.info }
-								</p>
-						</div>
-						<div className="socialHolder">
-							{member.social[0].linkedIn === "" ?
-								""
-								:
-								<a
-									className="social"
-									href={member.social[0].linkedIn}
-									target="_blank"
-									onClick={(e) => {e.preventDefault()}}
-								>
-									<i className="fab fa-linkedin"></i>
-								</a>
-							}
-							{member.social[0].twitter === "" ?
-								""
-								:
-								<a
-									className="social"
-									href="#"
-									onClick={(e) => {e.preventDefault()}}
-								>
-									<i className="fab fa-twitter"></i>
-								</a>
-							}
-							{member.social[0].email === "" ?
-								""
-								:
-								<a
-									className="social"
-									href={"mailto:" + member.social[0].email}
-									target="_self"
-								>
-									<i className="fas fa-envelope"></i>
-								</a>
-							}
-						</div>
-					</div>
-				))}
-			</div>
+      <div className="teamHolder">
+        {team.map((member, index) => (
+          <div className="teamColumn members" key={index}>
+            <div className="memberContainer">
+              <div className="avatarContainer">
+                <img src={member.image} alt="" className="avatar" />
+              </div>
+              <div className="memberFullname">
+                {member.fullname.map((name, index) => (
+                  <h2 className="memberFullnameMargin" key={index}>
+                    { name }
+                  </h2>
+                ))}
+              </div>
+              <div className="memberPositionCenter">
+                {member.positions && member.positions.map((position, index) => (
+                  <p className="memberPositionRow" key={index}>
+                    { position }
+                  </p>
+                ))}
+              </div>
+              <p className="memberInfo">
+                { member.info }
+              </p>
+            </div>
+            <div className="socialHolder">
+              {member.social[0].linkedIn === "" ?
+                ""
+                :
+                <a
+                  className="social"
+                  href={member.social[0].linkedIn}
+                  target="_blank"
+                  onClick={(e) => {e.preventDefault()}}
+                >
+                  <i className="fab fa-linkedin"></i>
+                </a>
+              }
+              {member.social[0].twitter === "" ?
+                ""
+                :
+                <a
+                  className="social"
+                  href="#"
+                  onClick={(e) => {e.preventDefault()}}
+                >
+                  <i className="fab fa-twitter"></i>
+                </a>
+              }
+              {member.social[0].email === "" ?
+                ""
+                :
+                <a
+                  className="social"
+                  href={"mailto:" + member.social[0].email}
+                  target="_self"
+                >
+                  <i className="fas fa-envelope"></i>
+                </a>
+              }
+            </div>
+          </div>
+        ))}
+      </div>
     );
   }
 }
 
 export default TeamMembers;
-
-
-
-
